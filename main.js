776ec324c033780787cf68689d48b9c30c6be8d6
dbrchrd <83125728+dbrchrd@users.noreply.github.com>
2021-05-24 08:38:30 +0200

    Update main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var wrd_plur = document.getElementById('wrd-plur');
+var nbr = document.getElementById('number');
+var text_area = document.getElementById('text-area');
+var text_area_str = [];
+var text_area_str_filt = [];
+const main = () => {
+  setTimeout(main, 33);
+}
+main();
